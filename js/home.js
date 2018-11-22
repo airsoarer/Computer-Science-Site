10ebb94283f28ea35e1140923ecff1609e2fdba8
Matt Williams <mwilliamsii233203@gmail.com>
2018-11-22 14:29:27 -0500

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -13,16 +13,108 @@
 
     function init(){
         firebase.initializeApp(config);
+        // Pull data for CS1 from Firebase
+        let ref = firebase.database().ref("Projects");
+        ref.on("child_added", snapshot => {
+            // Convert Data
+            let data = snapshot.val();
+            console.log(data);
+            
+            // Create html element
+            let div = document.createElement("div");
+            div.classList.add("row")
+            div.classList.add("project");
+
+            // Create div for screenshots
+            let photoDiv = document.createElement("div");
+            photoDiv.classList.add("col");
+            photoDiv.classList.add("s12");
+            photoDiv.classList.add("m4");
+            photoDiv.classList.add("offset-m1");
+            photoDiv.classList.add("photoDiv");
+            div.appendChild(photoDiv);
+
+            // Get Screenshots
+            for(var i = 0; i < data.FilePaths.length; i++){
+                firebase.storage().ref(data.FilePaths[i]).getDownloadURL().then(url => {
+                    let img = document.createElement("img");
+                    img.src = url;
+                    photoDiv.appendChild(img);
+                });
+            }
+
+            // Make div for info
+            let infoDiv = document.createElement("div");
+            infoDiv.classList.add("col");
+            infoDiv.classList.add("s12");
+            infoDiv.classList.add("m6");
+            div.appendChild(infoDiv);
+
+            // Title
+            let title = document.createElement("h4");
+            title.textContent = data.Title;
+            title.classList.add("col");
+            title.classList.add("s12")
+            title.classList.add("m12");
+            title.classList.add("title");
+            infoDiv.appendChild(title);
+
+            // Creators
+            let tempText = "Creators: ";
+            let creators = document.createElement("h5");
+
+            for(var i in data.Creators){
+                tempText += data.Creators[i] + ", ";
+            }
+
+            creators.textContent = tempText
+            creators.classList.add("col");
+            creators.classList.add("s12");
+            creators.classList.add("m12");
+            creators.classList.add("creators");
+            infoDiv.appendChild(creators);
+
+            // Class
+            let classTxt = document.createElement("h5");
+            classTxt.textContent = "Class: " + data.Class;
+            classTxt.classList.add("col");
+            classTxt.classList.add("s12");
+            classTxt.classList.add("m12");
+            classTxt.classList.add("class");
+            infoDiv.appendChild(classTxt);
+
+            // Description
+            let description = document.createElement("p");
+            description.textContent = data.Description;
+            description.classList.add("col");
+            description.classList.add("s12");
+            description.classList.add("m12");
+            description.classList.add("description");
+            infoDiv.appendChild(description);
+
+            if(data.Class === "CS1"){
+                $(".cs1Div").append(div);
+            }else if(data.Class === "CS2"){
+                $(".cs2Div").append(div);
+            }else if(data.Class === "CS3"){
+                $(".cs3Div").append(div);
+            }else if(data.Featured === true){
+                $(".homeDiv").append(div);
+            }else{
+
+            }
+        });
+
         // Arrow button event listener
         $(".scroll").on("click", scroll);
 
         // Show div button event listeners
         $(".admin").on("click", showAdmin);
-        $("#home").on("click", showHome);
-        $("#cs1").on("click", showCS1);
-        $("#cs2").on("click", showCS2);
-        $("#cs3").on("click", showCS3);
-        $("#addProject").on("click", showAddProject);
+        $(".home").on("click", showHome);
+        $(".cs1").on("click", showCS1);
+        $(".cs2").on("click", showCS2);
+        $(".cs3").on("click", showCS3);
+        $(".addProject").on("click", showAddProject);
 
         // Authentication button event listeners
         $("#login").on("click", login);
@@ -33,6 +125,9 @@
 
         // Add a creator input button event listener
         $("#addCreator").on("click", addCreator);
+
+        // Slidenav trigger
+        $(".sidenav").sidenav();
     }
 
     function addCreator(){
@@ -54,6 +149,7 @@
         let description = $("#description").val();
         let creatorArray = [];
 
+        // For loop to add all creators to creatorArray
         for(var i = 0; i < creators + 1; i++){
             creatorArray.push($(".creator" + i).val());
         }
@@ -61,7 +157,41 @@
         let classType = $("input[name=class]:checked").val();
         let projectType = $("input[name=projectType]:checked").val();
 
-        console.log(title, description, creatorArray, classType, projectType);
+        // Generate a random key to use as a uid
+        let key = firebase.database().ref("Project").push().key;
+
+        // Get Files
+        let files = $("#files")[0].files;
+        let filePaths = [];
+
+        // Loop through files
+        for(var i = 0; i < files.length; i++){
+            firebase.storage().ref("Projects/" + key + "/Screenshots/" + files[i].name).put(files[i], {contentType:files[i].type})   ;
+            filePaths.push("Projects/" + key + "/Screenshots/" + files[i].name);
+        }
+
+        // Send other data
+        firebase.database().ref("Projects/" + key).set({
+            Title:title,
+            Featured:false,
+            Description:description,
+            Creators:creatorArray,
+            Class:classType,
+            ProjectType:projectType,
+            FilePaths:filePaths,
+        });
+
+        setTimeout(() => {
+            $(".homeDiv").css("display", "block");
+            $("#loader").css("display", "none");
+        }, 2000);
+
+        $(".addProjectDiv").fadeOut(200);
+        $("#loader").css("display", "block");
+
+        // Empty inputs
+        $("#title").val("");
+        $("#description").val("");
     }
 
     // Logout function
@@ -122,6 +252,8 @@
         $(".cs3Div").css("display", "none");
         $(".addProjectDiv").css("display", "none")
 
+        $(".sidenav").sidenav("close");
+
         $("html, body").animate({
             scrollTop: $(".adminDiv").offset().top
         }, "slow")
@@ -136,6 +268,8 @@
         $(".cs3Div").css("display", "none");
         $(".addProjectDiv").css("display", "none")
 
+        $(".sidenav").sidenav("close");
+
         $("html, body").animate({
             scrollTop: $(".homeDiv").offset().top
         }, "slow")
@@ -143,12 +277,15 @@
 
     // Function to show and scroll to the cs1 div
     function showCS1(){
+        console.log("working");
         $(".adminDiv").css("display", "none");
         $(".homeDiv").css("display", "none");
         $(".cs1Div").css("display", "block");
         $(".cs2Div").css("display", "none");
         $(".cs3Div").css("display", "none");
         $(".addProjectDiv").css("display", "none")
+
+        $(".sidenav").sidenav("close");
 
         $("html, body").animate({
             scrollTop: $(".cs1Div").offset().top
@@ -164,6 +301,8 @@
         $(".cs3Div").css("display", "none");
         $(".addProjectDiv").css("display", "none");
 
+        $(".sidenav").sidenav("close");
+
         $("html, body").animate({
             scrollTop: $(".cs2Div").offset().top
         }, "slow")
@@ -178,6 +317,8 @@
         $(".cs3Div").css("display", "block");
         $(".addProjectDiv").css("display", "none")
 
+        $(".sidenav").sidenav("close");
+
         $("html, body").animate({
             scrollTop: $(".cs3Div").offset().top
         }, "slow")
@@ -191,6 +332,8 @@
         $(".cs2Div").css("display", "none");
         $(".cs3Div").css("display", "none");
         $(".addProjectDiv").css("display", "block")
+
+        $(".sidenav").sidenav("close");
 
         $("html, body").animate({
             scrollTop: $(".addProjectDiv").offset().top
